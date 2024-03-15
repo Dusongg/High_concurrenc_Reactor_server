de2ac46f87e73a6e8d4fdfca157d202e01ad9072
Dusongg <728869268@qq.com>
2024-03-15 14:43:30 +0800

    any

---
+++ example/any_test.cpp
@@ -0,0 +1,12 @@
+#include "any.h"
+#include <string>
+
+
+int main() {
+    any a;
+    a = 10;
+    std::cout << *a.get<int>() << std::endl;
+
+    a = std::string("xxx");
+    std::cout << *a.get<std::string>() << std::endl;
+}
